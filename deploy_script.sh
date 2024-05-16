1410a969098225dbccf55d753680575998b43aae
0xKaito <anuragchaturvedi978@gmail.com>
2024-05-16 23:14:11 +0530

    fix: deploy script

--- deploy_script.sh
+++ deploy_script.sh
@@ -12,7 +12,7 @@ ADMINCAP=$(jq '.types_id | .[1]' scripts/deployed_addresses.json)
 MINTERCAP=$(jq '.types_id | .[2]' scripts/deployed_addresses.json)
 DISPLAY=$(jq '.types_id | .[3]' scripts/deployed_addresses.json)
 PACKAGEID=$(jq '.PACKAGE_ID' scripts/deployed_addresses.json | tr -d '"')
-sui client call --package $PACKAGEID --module nft --function transfer_admin_cap --args $ADMINCAP $ADMIN --gas-budget 3038988
 sui client call --package $PACKAGEID --module nft --function transfer_publisher_object --args $ADMINCAP $PUBLISHERID $ADMIN --gas-budget 3038988
 sui client call --package $PACKAGEID --module nft --function transfer_upgrade_cap --args $ADMINCAP $UPGRADEID $ADMIN --gas-budget 3038988
 sui client call --package $PACKAGEID --module nft --function update_metadata --args $MINTERCAP $DISPLAY $NAME $DESCRIPTION --gas-budget 3038988
+sui client call --package $PACKAGEID --module nft --function transfer_admin_cap --args $ADMINCAP $ADMIN --gas-budget 3038988
