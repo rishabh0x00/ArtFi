871d17b18b18da81f3d7f0dcdd9042d95e7beb29
0xKaito <anuragchaturvedi978@gmail.com>
2024-05-27 17:20:34 +0530

    script: fix set up script for transfer object

--- setup_script.sh
+++ setup_script.sh
@@ -39,22 +39,22 @@ PACKAGEID=               # package id
 
 ADMIN=                   # new admin address
 
-sui client call --package $PACKAGEID --module base_nft --function transfer_object --type-args 0x2::package::Publisher --args $PUBLISHERID_nft $ADMIN --gas-budget 3038988
-sui client call --package $PACKAGEID --module base_nft --function transfer_object --type-args 0x2::package::Publisher --args $PUBLISHERID_gop_nft $ADMIN --gas-budget 3038988
-sui client call --package $PACKAGEID --module base_nft --function transfer_object --type-args 0x2::package::Publisher --args $PUBLISHERID_gap_nft $ADMIN --gas-budget 3038988
-sui client call --package $PACKAGEID --module base_nft --function transfer_object --type-args 0x2::package::Publisher --args $PUBLISHERID_trophy_nft $ADMIN --gas-budget 3038988
+sui client call --package $PACKAGEID --module base_nft --function transfer_publisher_object --args $PUBLISHERID_nft $ADMIN --gas-budget 3038988
+sui client call --package $PACKAGEID --module base_nft --function transfer_publisher_object --args $PUBLISHERID_gop_nft $ADMIN --gas-budget 3038988
+sui client call --package $PACKAGEID --module base_nft --function transfer_publisher_object --args $PUBLISHERID_gap_nft $ADMIN --gas-budget 3038988
+sui client call --package $PACKAGEID --module base_nft --function transfer_publisher_object --args $PUBLISHERID_trophy_nft $ADMIN --gas-budget 3038988
 echo "transfer publisher object completed successfully"
-sui client call --package $PACKAGEID --module base_nft --function transfer_object --type-args 0x2::package::UpgradeCap --args $UPGRADEID $ADMIN --gas-budget 3038988
+sui client call --package $PACKAGEID --module base_nft --function transfer_upgrade_object --args $UPGRADEID $ADMIN --gas-budget 3038988
 echo "transfer UpgradeCap object completed successfully"
 sui client call --package $PACKAGEID --module nft --function update_metadata --args $NFT_ADMINCAP $NFT_DISPLAY $NFT_NFTINFO $ARTFI_NAME $ARTFI_DESCRIPTION --gas-budget 3038988
 sui client call --package $PACKAGEID --module gop --function update_metadata --args $GOP_NFT_ADMINCAP $GOP_NFT_DISPLAY $GOP_NFTINFO $GOP_NAME $GOP_DESCRIPTION --gas-budget 3038988
 sui client call --package $PACKAGEID --module gap --function update_metadata --args $GAP_NFT_ADMINCAP $GAP_NFT_DISPLAY $GAP_NFTINFO $GAP_NAME $GAP_DESCRIPTION --gas-budget 3038988
 sui client call --package $PACKAGEID --module trophy --function update_metadata --args $TROPHY_NFT_ADMINCAP $TROPHY_NFT_DISPLAY $TROPHY_NFTINFO $TROPHY_NAME $TROPHY_DESCRIPTION --gas-budget 3038988
 echo "update metadata completed successfully"
-sui client call --package $PACKAGEID --module base_nft --function transfer_object --type-args "0x2::display::Display<$PACKAGEID::nft::ArtfiNFT>" --args $NFT_DISPLAY $ADMIN --gas-budget 3038988
-sui client call --package $PACKAGEID --module base_nft --function transfer_object --type-args "0x2::display::Display<$PACKAGEID::gop::GOPNFT>" --args $GOP_NFT_DISPLAY $ADMIN --gas-budget 3038988
-sui client call --package $PACKAGEID --module base_nft --function transfer_object --type-args "0x2::display::Display<$PACKAGEID::gap::GAPNFT>" --args $GAP_NFT_DISPLAY $ADMIN --gas-budget 3038988
-sui client call --package $PACKAGEID --module base_nft --function transfer_object --type-args "0x2::display::Display<$PACKAGEID::trophy::TrophyNFT>" --args $TROPHY_NFT_DISPLAY $ADMIN --gas-budget 3038988
+sui client call --package $PACKAGEID --module base_nft --function transfer_display_object --type-args "$PACKAGEID::nft::ArtfiNFT" --args $NFT_DISPLAY $ADMIN --gas-budget 3038988
+sui client call --package $PACKAGEID --module base_nft --function transfer_display_object --type-args "$PACKAGEID::gop::GOPNFT" --args $GOP_NFT_DISPLAY $ADMIN --gas-budget 3038988
+sui client call --package $PACKAGEID --module base_nft --function transfer_display_object --type-args "$PACKAGEID::gap::GAPNFT" --args $GAP_NFT_DISPLAY $ADMIN --gas-budget 3038988
+sui client call --package $PACKAGEID --module base_nft --function transfer_display_object --type-args "$PACKAGEID::trophy::TrophyNFT" --args $TROPHY_NFT_DISPLAY $ADMIN --gas-budget 3038988
 echo "transfer Display object completed successfully"
 sui client call --package $PACKAGEID --module base_nft --function transfer_admin_cap --args $BASE_NFT_ADMINCAP $ADMIN --gas-budget 3038988
 sui client call --package $PACKAGEID --module nft --function transfer_admin_cap --args $NFT_ADMINCAP $ADMIN --gas-budget 3038988
